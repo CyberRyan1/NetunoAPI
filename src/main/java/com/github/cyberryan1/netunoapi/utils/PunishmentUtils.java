8afddad958027a750468aff7f426457bccf8cf51
CyberRyan1 <52432798+CyberRyan1@users.noreply.github.com>
2022-07-31 17:06:27 -0500

    Added documentation comments

--- src/main/java/com/github/cyberryan1/netunoapi/utils/PunishmentUtils.java
+++ src/main/java/com/github/cyberryan1/netunoapi/utils/PunishmentUtils.java
@@ -8,17 +8,33 @@ import java.util.stream.Collectors;
 
 public class PunishmentUtils {
 
+    /**
+     * Returns if any of the punishments are active
+     * @param punishments List of {@link ANetunoPunishment}
+     * @return True if any of the punishments are active, false otherwise
+     */
     public static boolean anyActive( List<ANetunoPunishment> punishments ) {
         return punishments.stream()
                 .anyMatch( ANetunoPunishment::isActive );
     }
 
+    /**
+     * Returns all the punishments that are active
+     * @param punishments List of {@link ANetunoPunishment}
+     * @return List of {@link ANetunoPunishment} that are active
+     */
     public static List<ANetunoPunishment> getActive( List<ANetunoPunishment> punishments ) {
         return punishments.stream()
                 .filter( ANetunoPunishment::isActive )
                 .collect( Collectors.toList() );
     }
 
+    /**
+     * Returns the punishment that is active and has the longest length remaining
+     * @param punishments List of {@link ANetunoPunishment}
+     * @param type The type of punishment to search for
+     * @return The punishment that is active and has the longest length remaining
+     */
     public static ANetunoPunishment getHighestActive( List<ANetunoPunishment> punishments, PunishmentType type ) {
         return punishments.stream()
                 .filter( ( p ) -> p.getPunishmentType() == type )
@@ -27,6 +43,11 @@ public class PunishmentUtils {
                 .orElse( null );
     }
 
+    /**
+     * Returns the length remaining of the punishment before it expires, in seconds
+     * @param punishment The punishment to get the length remaining of
+     * @return The length remaining, in seconds
+     */
     public static long getTimestampLengthRemaining( ANetunoPunishment punishment ) {
         if ( punishment.isActive() == false ) { return -1L; }
         return punishment.getLength() - ( TimeUtils.getCurrentTimestamp() - punishment.getTimestamp() );
