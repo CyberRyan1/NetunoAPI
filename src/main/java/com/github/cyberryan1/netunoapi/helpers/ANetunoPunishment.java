ed6188670c0475aa27a2878b0f6fc364e9c60479
CyberRyan1 <52432798+CyberRyan1@users.noreply.github.com>
2022-07-31 11:53:37 -0500

    Added hasNoLength method to PunishmentType enum

--- src/main/java/com/github/cyberryan1/netunoapi/helpers/ANetunoPunishment.java
+++ src/main/java/com/github/cyberryan1/netunoapi/helpers/ANetunoPunishment.java
@@ -5,13 +5,9 @@ import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 
 public class ANetunoPunishment extends ANetunoPunishmentData {
-
-    public static final List<PunishmentType> PUNS_NO_LENGTH = List.of( PunishmentType.WARN, PunishmentType.KICK,
-            PunishmentType.UNMUTE, PunishmentType.UNBAN, PunishmentType.UNIPMUTE, PunishmentType.UNIPBAN );
 
     public ANetunoPunishment( int id, PunishmentType punishmentType, String playerUuid, String staffUuid, long length, long timestamp, String reason, ArrayList<String> altsPunished ) {
         super( id, punishmentType, playerUuid, staffUuid, length, timestamp, reason, altsPunished );
@@ -39,7 +35,7 @@ public class ANetunoPunishment extends ANetunoPunishmentData {
     public ANetunoPunishment() {}
 
     public boolean isActive() {
-        if ( PUNS_NO_LENGTH.contains( super.punishmentType ) ) { return false; }
+        if ( super.punishmentType.hasNoLength() ) { return false; }
         return ( TimeUtils.getCurrentTimestamp() - super.timestamp ) < super.length;
     }
 
--- src/main/java/com/github/cyberryan1/netunoapi/helpers/PunishmentType.java
+++ src/main/java/com/github/cyberryan1/netunoapi/helpers/PunishmentType.java
@@ -1,14 +1,23 @@
 package com.github.cyberryan1.netunoapi.helpers;
 
 public enum PunishmentType {
-    WARN,
-    KICK,
-    MUTE,
-    UNMUTE,
-    BAN,
-    UNBAN,
-    IPMUTE,
-    UNIPMUTE,
-    IPBAN,
-    UNIPBAN;
+    WARN ( true ),
+    KICK ( true ),
+    MUTE ( false ),
+    UNMUTE ( true ),
+    BAN ( false ),
+    UNBAN ( true ),
+    IPMUTE ( false ),
+    UNIPMUTE ( true ),
+    IPBAN ( false ),
+    UNIPBAN ( true );
+
+    private boolean hasNoLength;
+    PunishmentType( boolean hasNoLength ) {
+        this.hasNoLength = hasNoLength;
+    }
+
+    public boolean hasNoLength() {
+        return hasNoLength;
+    }
 }
