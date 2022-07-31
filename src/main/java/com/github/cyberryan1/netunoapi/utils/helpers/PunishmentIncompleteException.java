a2e637ebe1bcdf641882ea879512ef0c1105fab0
CyberRyan1 <52432798+CyberRyan1@users.noreply.github.com>
2022-07-31 14:49:17 -0500

    Created the PunishmentIncompleteException

---
+++ src/main/java/com/github/cyberryan1/netunoapi/utils/helpers/PunishmentIncompleteException.java
@@ -0,0 +1,7 @@
+package com.github.cyberryan1.netunoapi.utils.helpers;
+
+public class PunishmentIncompleteException extends RuntimeException {
+    public PunishmentIncompleteException( String message ) {
+        super( message );
+    }
+}
