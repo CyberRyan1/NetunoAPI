8431cd5594966d6aeb972d6bfff2c0424aae2426
CyberRyan1 <52432798+CyberRyan1@users.noreply.github.com>
2022-07-31 14:17:54 -0500

    Added the isIpPunishment() method

--- src/main/java/com/github/cyberryan1/netunoapi/punishments/PunishmentType.java
+++ src/main/java/com/github/cyberryan1/netunoapi/punishments/PunishmentType.java
@@ -1,23 +1,29 @@
 package com.github.cyberryan1.netunoapi.punishments;
 
 public enum PunishmentType {
-    WARN ( true ),
-    KICK ( true ),
-    MUTE ( false ),
-    UNMUTE ( true ),
-    BAN ( false ),
-    UNBAN ( true ),
-    IPMUTE ( false ),
-    UNIPMUTE ( true ),
-    IPBAN ( false ),
-    UNIPBAN ( true );
+    WARN ( true, false ),
+    KICK ( true, false ),
+    MUTE ( false, false ),
+    UNMUTE ( true, false ),
+    BAN ( false, false ),
+    UNBAN ( true, false ),
+    IPMUTE ( false, true ),
+    UNIPMUTE ( true, true ),
+    IPBAN ( false, true ),
+    UNIPBAN ( true, true );
 
     private boolean hasNoLength;
-    PunishmentType( boolean hasNoLength ) {
+    private boolean ipPunishment;
+    PunishmentType( boolean hasNoLength, boolean ipPunishment ) {
         this.hasNoLength = hasNoLength;
+        this.ipPunishment = ipPunishment;
     }
 
     public boolean hasNoLength() {
         return hasNoLength;
+    }
+
+    public boolean isIpPunishment() {
+        return ipPunishment;
     }
 }
