a1af9314a72be01a79c0e04bebf675b9095db2c8
CyberRyan1 <52432798+CyberRyan1@users.noreply.github.com>
2022-07-31 21:52:02 -0500

    Added documentation comments

--- src/main/java/com/github/cyberryan1/netunoapi/utils/helpers/ExpiringCache.java
+++ src/main/java/com/github/cyberryan1/netunoapi/utils/helpers/ExpiringCache.java
@@ -13,11 +13,23 @@ public class ExpiringCache<T> {
     private Map<T, Long> cache = new HashMap<>();
     private long expirationTime = 300; // 5 minutes
 
+    /**
+     * Adds a new entry to the cache
+     * @param key The new entry
+     */
     public void add( T key ) {
         cache.put( key, TimeUtils.getCurrentTimestamp() );
     }
 
+    /**
+     * Checks if an entry is expired. If they key is expired and the
+     * cache still contains the key, it is automatically removed. If
+     * the key is not in the cache, false is returned
+     * @param key The key to check
+     * @return True if the key is expired or not in the cache, false otherwise
+     */
     public boolean isExpired( T key ) {
+        if ( cache.containsKey( key ) == false ) { return true; }
         if ( TimeUtils.getCurrentTimestamp() - cache.get( key ) > expirationTime ) {
             cache.remove( key );
             return true;
@@ -25,6 +37,12 @@ public class ExpiringCache<T> {
         return false;
     }
 
+    /**
+     * Gets all elements in the cache that are not expired and also
+     * satisfy the given predicate.
+     * @param predicate The predicate to check with
+     * @return A list of all unexpired elements that satisfy the predicate
+     */
     public List<T> searchForMany( Predicate<? super T> predicate ) {
         return cache.keySet().stream()
                 .filter( ( t ) -> isExpired( t ) == false )
@@ -32,20 +50,38 @@ public class ExpiringCache<T> {
                 .collect( Collectors.toList() );
     }
 
+    /**
+     * Gets the first element in the cache that is not expired and
+     * also satisfies the given predicate.
+     * @param predicate The predicate to check with
+     * @return The first unexpired element that satisfies the predicate, null if none were found
+     */
     public T searchForOne( Predicate<? super T> predicate ) {
         List<T> list = searchForMany( predicate );
         if ( list.size() == 0 ) { return null; }
         return list.get( 0 );
     }
 
+    /**
+     * Removes the given element from the cache
+     * @param key The element to remove
+     */
     public void remove( T key ) {
         cache.remove( key );
     }
 
+    /**
+     * Sets the expiration timer of the cache, in seconds.
+     * The default is 300 seconds (5 minutes).
+     * @param expirationTime The expiration time in seconds
+     */
     public void setExpirationTime( long expirationTime ) {
         this.expirationTime = expirationTime;
     }
 
+    /**
+     * @return The expiration time of the cache, in seconds
+     */
     public long getExpirationTime() {
         return expirationTime;
     }
