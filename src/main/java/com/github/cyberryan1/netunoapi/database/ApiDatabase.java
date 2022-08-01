5ecadd79f7bb85f22b92a9aec9a8d4c806d6c5f1
CyberRyan1 <52432798+CyberRyan1@users.noreply.github.com>
2022-08-01 03:22:06 -0500

    Created the ApiDatabase class

---
+++ src/main/java/com/github/cyberryan1/netunoapi/database/ApiDatabase.java
@@ -0,0 +1,68 @@
+package com.github.cyberryan1.netunoapi.database;
+
+/**
+ * This class is used to manage all the different database instance classes.
+ */
+public class ApiDatabase {
+
+    private static AltsDatabase altsDatabase = new AltsDatabase();
+    private static PunishmentsDatabase punDatabase = new PunishmentsDatabase();
+    private static RandomDatabase randomDatabase = new RandomDatabase();
+    private static ReportsDatabase reportsDatabase = new ReportsDatabase();
+
+    /**
+     * @return The {@link AltsDatabase} instance
+     */
+    public static AltsDatabase getAltsDatabase() {
+        return altsDatabase;
+    }
+
+    /**
+     * @return The {@link AltsDatabase} instance
+     */
+    public static AltsDatabase getAlts() {
+        return altsDatabase;
+    }
+
+    /**
+     * @return The {@link PunishmentsDatabase} instance
+     */
+    public static PunishmentsDatabase getPunDatabase() {
+        return punDatabase;
+    }
+
+    /**
+     * @return The {@link PunishmentsDatabase} instance
+     */
+    public static PunishmentsDatabase getPun() {
+        return punDatabase;
+    }
+
+    /**
+     * @return The {@link RandomDatabase} instance
+     */
+    public static RandomDatabase getRandomDatabase() {
+        return randomDatabase;
+    }
+
+    /**
+     * @return The {@link RandomDatabase} instance
+     */
+    public static RandomDatabase getRandom() {
+        return randomDatabase;
+    }
+
+    /**
+     * @return The {@link ReportsDatabase} instance
+     */
+    public static ReportsDatabase getReportsDatabase() {
+        return reportsDatabase;
+    }
+
+    /**
+     * @return The {@link ReportsDatabase} instance
+     */
+    public static ReportsDatabase getReports() {
+        return reportsDatabase;
+    }
+}
