e457d60ca1401e5af80c9faf338433de08b6d548
CyberRyan1 <52432798+CyberRyan1@users.noreply.github.com>
2022-07-31 21:53:48 -0500

    Added documentation comments

--- src/main/java/com/github/cyberryan1/netunoapi/punishments/ANetunoPunishment.java
+++ src/main/java/com/github/cyberryan1/netunoapi/punishments/ANetunoPunishment.java
@@ -14,15 +14,25 @@ public class ANetunoPunishment extends ANetunoPunishmentData {
 
     public ANetunoPunishment() {}
 
+    /**
+     * @return true if the punishment is still active, false if
+     * it is not active or if the punishment has no length by default
+     */
     public boolean isActive() {
         if ( super.punishmentType.hasNoLength() ) { return false; }
         return ( TimeUtils.getCurrentTimestamp() - super.timestamp ) < super.length;
     }
 
+    /**
+     * @return The player as an {@link OfflinePlayer}
+     */
     public OfflinePlayer getPlayer() {
         return Bukkit.getOfflinePlayer( UUID.fromString( super.playerUuid ) );
     }
 
+    /**
+     * @return The staff as an {@link OfflinePlayer}
+     */
     public OfflinePlayer getStaff() {
         return Bukkit.getOfflinePlayer( UUID.fromString( super.staffUuid ) );
     }
