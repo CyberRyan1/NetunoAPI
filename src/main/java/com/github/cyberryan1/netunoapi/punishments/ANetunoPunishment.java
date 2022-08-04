56ccf6febbde9bb7f473267e6b8d1d28c039798f
CyberRyan1 <52432798+CyberRyan1@users.noreply.github.com>
2022-08-03 19:46:42 -0500

    Added the ensureValid method

--- src/main/java/com/github/cyberryan1/netunoapi/punishments/ANetunoPunishment.java
+++ src/main/java/com/github/cyberryan1/netunoapi/punishments/ANetunoPunishment.java
@@ -1,6 +1,7 @@
 package com.github.cyberryan1.netunoapi.punishments;
 
 import com.github.cyberryan1.netunoapi.utils.TimeUtils;
+import com.github.cyberryan1.netunoapi.utils.helpers.ClassIncompleteException;
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
 
@@ -35,5 +36,17 @@ public class ANetunoPunishment extends ANetunoPunishmentData {
      */
     public OfflinePlayer getStaff() {
         return Bukkit.getOfflinePlayer( UUID.fromString( super.staffUuid ) );
+    }
+
+    public void ensureValid( boolean requireValidId ) {
+        if ( requireValidId && this.id <= 0 ) { throw new ClassIncompleteException( "Punishment incomplete: Punishment ID must be greater than zero" ); }
+        if ( requireValidId == false && this.id > 0 ) { throw new ClassIncompleteException( "Punishment incomplete: Punishment ID must be less than or equal to zero" ); }
+        if ( this.punishmentType == null ) { throw new ClassIncompleteException( "Punishment incomplete: Punishment type cannot be null" ); }
+        if ( this.playerUuid == null ) { throw new ClassIncompleteException( "Punishment incomplete: Player UUID cannot be null" ); }
+        if ( this.staffUuid == null ) { throw new ClassIncompleteException( "Punishment incomplete: Staff UUID cannot be null" ); }
+        if ( this.length <= 0 && this.punishmentType.hasNoLength() == false ) { throw new ClassIncompleteException( "Punishment incomplete: Length must be greater than zero seconds" ); }
+        if ( this.timestamp <= 0 ) { throw new ClassIncompleteException( "Punishment incomplete: Timestamp must be greater than zero" ); }
+        if ( this.reason == null ) { throw new ClassIncompleteException( "Punishment incomplete: Reason cannot be null" ); }
+        if ( this.punishmentType.isIpPunishment() && this.referencePunId <= 0 ) { throw new ClassIncompleteException( "Punishment incomplete: Reference Punishment ID must be greater than zero for IP punishments" ); }
     }
 }
