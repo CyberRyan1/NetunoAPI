3d750afd26716e23a01ac1d2c37461868bcbb630
CyberRyan1 <52432798+CyberRyan1@users.noreply.github.com>
2022-08-03 19:42:11 -0500

    Added the hasNoReason variable

--- src/main/java/com/github/cyberryan1/netunoapi/punishments/PunishmentType.java
+++ src/main/java/com/github/cyberryan1/netunoapi/punishments/PunishmentType.java
@@ -1,26 +1,32 @@
 package com.github.cyberryan1.netunoapi.punishments;
 
 public enum PunishmentType {
-    WARN ( true, false ),
-    KICK ( true, false ),
-    MUTE ( false, false ),
-    UNMUTE ( true, false ),
-    BAN ( false, false ),
-    UNBAN ( true, false ),
-    IPMUTE ( false, true ),
-    UNIPMUTE ( true, true ),
-    IPBAN ( false, true ),
-    UNIPBAN ( true, true );
+    WARN ( true, false,false ),
+    KICK ( true, false, false ),
+    MUTE ( false, false, false ),
+    UNMUTE ( true, true, false ),
+    BAN ( false, false, false ),
+    UNBAN ( true, true, false ),
+    IPMUTE ( false, false, true ),
+    UNIPMUTE ( true, true, true ),
+    IPBAN ( false, false, true ),
+    UNIPBAN ( true, true, true );
 
     private boolean hasNoLength;
+    private boolean hasNoReason;
     private boolean ipPunishment;
-    PunishmentType( boolean hasNoLength, boolean ipPunishment ) {
+    PunishmentType( boolean hasNoLength, boolean hasNoReason, boolean ipPunishment ) {
         this.hasNoLength = hasNoLength;
+        this.hasNoReason = hasNoReason;
         this.ipPunishment = ipPunishment;
     }
 
     public boolean hasNoLength() {
         return hasNoLength;
+    }
+
+    public boolean hasNoReason() {
+        return hasNoReason;
     }
 
     public boolean isIpPunishment() {
