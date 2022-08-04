37f0e339365d014ea4625e118dee7d06d48069cf
CyberRyan1 <52432798+CyberRyan1@users.noreply.github.com>
2022-08-03 23:29:21 -0500

    Fixed typo

--- src/main/java/com/github/cyberryan1/netunoapi/database/RandomDatabase.java
+++ src/main/java/com/github/cyberryan1/netunoapi/database/RandomDatabase.java
@@ -5,7 +5,7 @@ package com.github.cyberryan1.netunoapi.database;
  * between plugin reloads that is not already stored in one of the other
  * databases. <br><br>
  * <b>(!) The use of the RandomDatabase is to be implemented on your own.
- * You pretty much <u>no use</u> for this if you are not the Netuno developer.</b>
+ * You pretty much have <u>no use</u> for this if you are not the Netuno developer.</b>
  */
 public interface RandomDatabase {
 
