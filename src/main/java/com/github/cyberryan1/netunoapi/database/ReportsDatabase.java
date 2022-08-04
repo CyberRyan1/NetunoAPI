c181d36b4e9f8be668c02c9f1d0b53767ef5401d
CyberRyan1 <52432798+CyberRyan1@users.noreply.github.com>
2022-08-04 16:39:19 -0500

    Removed the forceGetReports() methods

--- src/main/java/com/github/cyberryan1/netunoapi/database/ReportsDatabase.java
+++ src/main/java/com/github/cyberryan1/netunoapi/database/ReportsDatabase.java
@@ -29,10 +29,6 @@ public interface ReportsDatabase {
     /**
      * Searches for all reports in the database and in the cache
      * that have the given player has the player. <br>
-     * <i><b>Note:</b> This will search for all reports from the
-     * cache first, and if none are found, then the database will
-     * be searched. If you want to search just the database, then
-     * use the {@link #forceGetReports( OfflinePlayer )} method</i>
      * @param player The player to search for
      * @return A {@link List<NReport>} of all reports for the player
      */
@@ -41,34 +37,10 @@ public interface ReportsDatabase {
     /**
      * Searches for all reports in the database and in the cache
      * that have the given player UUID has the player. <br>
-     * <i><b>Note:</b> This will search for all reports from the
-     * cache first, and if none are found, then the database will
-     * be searched. If you want to search just the database, then
-     * use the {@link #forceGetReports( String )} method</i>
      * @param playerUuid The player UUID to search for
      * @return A {@link List<NReport>} of all reports for the player
      */
     List<NReport> getReports( String playerUuid );
-
-    /**
-     * Searches for all reports in just the database, not in the
-     * cache, that have the given player as the player. <br>
-     * <i>If you want to search the cache first then the database,
-     * use the {@link #getReports( String )} method</i>
-     * @param player The {@link OfflinePlayer} to search for
-     * @return A {@link List<NReport>} of all reports for the player
-     */
-    List<NReport> forceGetReports( OfflinePlayer player );
-
-    /**
-     * Searches for all reports in just the database, not in the
-     * cache, that have the given player UUID as the player's UUID. <br>
-     * <i>If you want to search the cache first then the database,
-     * use the {@link #getReports( String )} method</i>
-     * @param playerUuid The player UUID to search for
-     * @return A {@link List<NReport>} of all reports for the player
-     */
-    List<NReport> forceGetReports( String playerUuid );
 
     /**
      * Deletes a report with the given id.
