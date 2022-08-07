a7b4e82b66cc00079fc082ce1772ca83d9fb98d8
CyberRyan1 <52432798+CyberRyan1@users.noreply.github.com>
2022-08-06 21:53:38 -0500

    Added java docs

--- src/main/java/com/github/cyberryan1/netunoapi/NetunoApi.java
+++ src/main/java/com/github/cyberryan1/netunoapi/NetunoApi.java
@@ -6,17 +6,37 @@ import com.github.cyberryan1.netunoapi.models.players.NPlayerLoader;
 
 public interface NetunoApi {
 
+    /**
+     * Gets the database connection
+     * @return The {@link DatabaseConnection} instance
+     */
     DatabaseConnection getConnectionManager();
 
+    /**
+     * Alias for {@link #getConnectionManager()}
+     * @return The {@link DatabaseConnection} instance
+     */
     default DatabaseConnection getConn() {
         return getConnectionManager();
     }
 
+    /**
+     * Gets the database manager
+     * @return The {@link NetunoDatabases} instance
+     */
     NetunoDatabases getDatabaseManager();
 
+    /**
+     * Alias for {@link #getDatabaseManager()}
+     * @return The {@link NetunoDatabases} instance
+     */
     default NetunoDatabases getDatabases() {
         return getDatabaseManager();
     }
 
+    /**
+     * Gets the player loader
+     * @return The {@link NPlayerLoader} instance
+     */
     NPlayerLoader getPlayerLoader();
 }
