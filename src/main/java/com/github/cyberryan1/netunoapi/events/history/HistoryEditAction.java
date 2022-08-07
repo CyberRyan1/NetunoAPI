b63a68be1f8eb5c2a79e43cbef8defa34521d192
CyberRyan1 <52432798+CyberRyan1@users.noreply.github.com>
2022-08-07 00:40:18 -0500

    Created the HistoryEditAction enum

---
+++ src/main/java/com/github/cyberryan1/netunoapi/events/history/HistoryEditAction.java
@@ -0,0 +1,9 @@
+package com.github.cyberryan1.netunoapi.events.history;
+
+/**
+ * Represents an action that can be performed on a history entry
+ */
+public enum HistoryEditAction {
+    EDIT_LENGTH,
+    EDIT_REASON;
+}
