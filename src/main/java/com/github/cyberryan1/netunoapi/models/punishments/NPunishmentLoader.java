8481c230345757ce6d7605ac8319d8f2c6d79f1f
CyberRyan1 <52432798+CyberRyan1@users.noreply.github.com>
2022-08-06 23:58:35 -0500

    Added javadocs

--- src/main/java/com/github/cyberryan1/netunoapi/models/punishments/NPunishmentLoader.java
+++ src/main/java/com/github/cyberryan1/netunoapi/models/punishments/NPunishmentLoader.java
@@ -2,7 +2,15 @@ package com.github.cyberryan1.netunoapi.models.punishments;
 
 public interface NPunishmentLoader {
 
+    /**
+     * @return A new {@link NPunishment} object
+     */
     NPunishment createPunishment();
 
+    /**
+     * Creates a {@link NPrePunishment} from the given {@link NPunishment}
+     * @param punishment The {@link NPunishment} to create the {@link NPrePunishment} from
+     * @return The created {@link NPrePunishment}
+     */
     NPrePunishment createPrePunishment( NPunishment punishment );
 }
